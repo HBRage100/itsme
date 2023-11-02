3688c7883059b9d6561c79144f46db96a84024b0
HB Rage <HBRage100@users.noreply.github.com>
2023-11-03 00:09:50 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -25,35 +25,69 @@ const Home = (props) => {
         } lg:px-40 md:px-12 px-4 grid lg:grid-cols-2 grid-cols-1 lg:pb-0 pb-32 relative`}
       >
         <div className="max-md:order-2 md:pt-32 pt-16">
-          <span
+          <m.span
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.2 },
+            }}
             className={`inline-flex text-5xl font-medium gap-5 ${
               props.mode === false ? "text-steelGray-950" : "text-white"
             }`}
           >
             <img src={helloHand} alt="Hello" className="animate-waving-hand" />I
             am
-          </span>
-          <h1
+          </m.span>
+          <m.h1
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.3 },
+            }}
             className={`text-6xl font-semibold py-4 ${
               props.mode === false ? "text-steelGray-950" : "text-white"
             }`}
           >
             <TypingEffect />
-          </h1>
-          <div className="flex items-center text-studio-700">
+          </m.h1>
+          <m.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.4 },
+            }}
+            className="flex items-center text-studio-700"
+          >
             <hr class="lg:w-28 w-20 border-t border-2 border-studio-700 mr-4" />
             <span className="text-2xl">Web Developer</span>
-          </div>
-          <p
+          </m.div>
+          <m.p
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.5 },
+            }}
             className={`lg:pr-60 py-4 text-lg ${
               props.mode === false ? "text-steelGray-950" : "text-white"
             }`}
           >
             I am a Web Developer with a Excellent track record in designing,
             developing websites, and managing databases.
-          </p>
+          </m.p>
           {/* button */}
-          <div className="lg:inline-flex flex-wrap items-center justify-center flex gap-5 pt-4">
+          <m.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.6 },
+            }}
+            className="lg:inline-flex flex-wrap items-center justify-center flex gap-5 pt-4"
+          >
             <Link
               activeClass="active"
               to="contact"
@@ -74,9 +108,17 @@ const Home = (props) => {
             >
               <WhiteBtn btntxt="My Work" />
             </Link>
-          </div>
+          </m.div>
           {/* Follow button */}
-          <div className="lg:pt-16 pt-8 flex items-center">
+          <m.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.7 },
+            }}
+            className="lg:pt-16 pt-8 flex items-center"
+          >
             <p
               className={`text-lg inline-block ${
                 props.mode === false ? "text-steelGray-950" : "text-white"
@@ -111,7 +153,7 @@ const Home = (props) => {
                 <FaLinkedinIn size={30} />
               </span>
             </a>
-          </div>
+          </m.div>
           <Link
             activeClass="active"
             to="about"
@@ -129,31 +171,87 @@ const Home = (props) => {
           </Link>
         </div>
         <div className="relative my-auto max-md:order-1 max-md:pt-32 md:pt-10 max-sm:pt-24">
-          <div className="bg-white shadow-2xl lg:mt-16 lg:mx-56 max-md:mx-40 md:mx-32 max-sm:mx-16 w-24 h-24 flex justify-center items-center rounded-2xl">
+          <m.div
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.2 },
+            }}
+            className="bg-white shadow-2xl lg:mt-16 lg:mx-56 max-md:mx-40 md:mx-32 max-sm:mx-16 w-24 h-24 flex justify-center items-center rounded-2xl"
+          >
             <img src={figma} alt="VS Code" className="w-16 h-16" />
-          </div>
-          <div className="bg-white shadow-2xl lg:ml-[70%] lg:-mt-10 ml-[70%] -mt-20 w-20 h-20 absolute flex justify-center items-center rounded-2xl">
+          </m.div>
+          <m.div
+            initial={{ opacity: 0, y: -100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.4 },
+            }}
+            className="bg-white shadow-2xl lg:ml-[70%] lg:-mt-10 ml-[70%] -mt-20 w-20 h-20 absolute flex justify-center items-center rounded-2xl"
+          >
             <img src={fireFox} alt="VS Code" className="w-12 h-12" />
-          </div>
-          <div className="bg-white shadow-2xl lg:mx-40 max-md:mx-20 md:mx-16 max-sm:mx-2 mt-16 w-20 h-20 absolute flex justify-center items-center rounded-2xl">
+          </m.div>
+          <m.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.5, delay: 0.2 },
+            }}
+            className="bg-white shadow-2xl lg:mx-40 max-md:mx-20 md:mx-16 max-sm:mx-2 mt-16 w-20 h-20 absolute flex justify-center items-center rounded-2xl"
+          >
             <img src={tailWind} alt="VS Code" className="w-12 h-12" />
-          </div>
-          <div className="bg-white shadow-2xl mx-auto mt-4 lg:w-40 lg:h-40 w-32 h-32 flex justify-center items-center rounded-2xl">
+          </m.div>
+          <m.div
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{
+              opacity: 1,
+              scale: 1,
+              transition: { duration: 0.5, delay: 0.5 },
+            }}
+            className="bg-white shadow-2xl mx-auto mt-4 lg:w-40 lg:h-40 w-32 h-32 flex justify-center items-center rounded-2xl"
+          >
             <img
               src={vsCode}
               alt="VS Code"
               className="lg:w-32 lg:h-32 w-28 h-28"
             />
-          </div>
-          <div className="bg-white shadow-2xl lg:mx-48 max-md:mx-28 md:mx-24 max-sm:mx-6 mt-4 w-28 h-28 flex justify-center items-center rounded-2xl">
+          </m.div>
+          <m.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.6 },
+            }}
+            className="bg-white shadow-2xl lg:mx-48 max-md:mx-28 md:mx-24 max-sm:mx-6 mt-4 w-28 h-28 flex justify-center items-center rounded-2xl"
+          >
             <img src={reactIcon} alt="VS Code" className="w-22 h-22" />
-          </div>
-          <div className="bg-white shadow-2xl lg:ml-[64%] max-md:ml-[75%] md:ml-[65%] -mt-60 w-14 h-14 absolute flex justify-center items-center rounded-2xl">
+          </m.div>
+          <m.div
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.5, delay: 0.2 },
+            }}
+            className="bg-white shadow-2xl lg:ml-[64%] max-md:ml-[75%] md:ml-[65%] -mt-60 w-14 h-14 absolute flex justify-center items-center rounded-2xl"
+          >
             <img src={gitHub} alt="VS Code" className="w-10 h-10" />
-          </div>
-          <div className="bg-white shadow-2xl ml-[50%] -mt-24 w-24 h-24 absolute flex justify-center items-center rounded-2xl">
+          </m.div>
+          <m.div
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 0.5, delay: 0.6 },
+            }}
+            className="bg-white shadow-2xl ml-[50%] -mt-24 w-24 h-24 absolute flex justify-center items-center rounded-2xl"
+          >
             <img src={photoShop} alt="VS Code" className="w-16 h-16" />
-          </div>
+          </m.div>
         </div>
       </section>
     </>
